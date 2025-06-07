7026b6c013912bdd6fa0f70d899a74acb8e988a2
Thinha1 <Thinhanhdang014@gmail.com>
2025-06-07 21:51:47 +0700

    init project

---
+++ src/test/java/vn/thinh/Studentms/StudentmsApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.thinh.Studentms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
